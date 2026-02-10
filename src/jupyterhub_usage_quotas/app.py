b6045e644e22397910dab6be949bfe63834a09da
jnywong <jnywong.pro@gmail.com>
2026-02-10 12:42:33 +0000

    feat: generate default config

--- src/jupyterhub_usage_quotas/app.py
+++ src/jupyterhub_usage_quotas/app.py
@@ -1,8 +1,10 @@
 import logging.config
+import os
+import signal
 
 import uvicorn
 from fastapi import FastAPI
-from traitlets import Integer, Unicode
+from traitlets import Bool, Integer, Unicode
 from traitlets.config import Application, Instance
 
 from jupyterhub_usage_quotas.config import Quotas
@@ -30,6 +32,7 @@ class QuotasApp(Application):
     config_file = Unicode(
         "jupyterhub_usage_quotas_config.py", help="The config file to load"
     ).tag(config=True)
+    generate_config = Bool(False, help="Generate default config file").tag(config=True)
 
     # Configurable traits
 
@@ -43,6 +46,27 @@ class QuotasApp(Application):
         "f": "QuotasApp.config_file",
         "config": "QuotasApp.config_file",
     }
+
+    flags = {
+        "generate-config": (
+            {"QuotasApp": {"generate_config": True}},
+            "Generate default config file",
+        )
+    }
+
+    def initialize(self, *args, **kwargs):
+        super().initialize(*args, **kwargs)
+        self._format_logs()
+        if self.generate_config:
+            config_text = self.generate_config_file()
+            if isinstance(config_text, bytes):
+                config_text = config_text.decode("utf8")
+            self.log.info(f"Writing default config to: {self.config_file}")
+            with open(self.config_file, mode="w") as f:
+                f.write(config_text)
+            self.stop()
+
+        return
 
     def _build_app(self) -> FastAPI:
         app = FastAPI()
@@ -66,9 +90,9 @@ class QuotasApp(Application):
         self.log.propagate = True
 
     def start(self):
+        self.initialize()
         self.app = self._build_app()
         self.load_config_file(self.config_file)
-        self._format_logs()
         self.log.info(f"Starting server on {self.server_ip}:{self.server_port}")
 
         uvicorn.run(
@@ -78,6 +102,10 @@ class QuotasApp(Application):
             log_level=self.log_level,
             log_config=None,  # use LOGGING_CONFIG and not uvicorn log config
         )
+
+    def stop(self):
+        os.kill(os.getpid(), signal.SIGTERM)
+        return
 
 
 def main():
