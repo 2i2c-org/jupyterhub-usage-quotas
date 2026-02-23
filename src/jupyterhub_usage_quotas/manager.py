d634d6826da5bd93b6bf4c63a1e58b0c04238f64
jnywong <jnywong.pro@gmail.com>
2026-02-23 11:55:38 +0000

    feat: add hub api client

--- src/jupyterhub_usage_quotas/manager.py
+++ src/jupyterhub_usage_quotas/manager.py
@@ -3,7 +3,7 @@ from typing import Any, Optional
 
 from tornado import web
 
-from jupyterhub_usage_quotas.client import PrometheusClient
+from jupyterhub_usage_quotas.client import HubAPIClient, PrometheusClient
 from jupyterhub_usage_quotas.config import UsageQuotaConfig
 
 
@@ -11,17 +11,29 @@ class UsageQuotaManager(UsageQuotaConfig):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        hub_ip = self.config["JupyterHub"]["ip"]
+        hub_port = self.config["JupyterHub"]["port"]
+        self.hub_api_client = HubAPIClient(hub_url=f"http://{hub_ip}:{hub_port}")
         self.prometheus_client = PrometheusClient(self.prometheus_url)
+
+    async def resolve_policy(self, user):
+        """
+        Resolve which quota policy applies to the user.
+        """
+        data_user = await self.hub_api_client.query("users")
+        return data_user
 
     async def enforce(self, user):
         usage_metric = self.prometheus_usage_metrics["memory"]
         pattern = r"(\{.*?)(\})"
         repl = rf"\1, pod='jupyter-{user}'\2"
         promql = re.sub(pattern, repl, usage_metric)
-        data = await self.prometheus_client.query(promql)
-        print(f"{data=}")
+        data_prometheus = await self.prometheus_client.query(promql)
+        print(f"{data_prometheus=}")
 
         # TODO: apply quota logic
+        data_user = await self.resolve_policy(user)
+        print(f"{data_user}")
 
         return True
 
