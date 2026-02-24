47939d30b38d0aa940f4807015fe723a0aefe3d5
jnywong <jnywong.pro@gmail.com>
2026-02-24 14:37:21 +0000

    feat: add policy resolver

--- src/jupyterhub_usage_quotas/manager.py
+++ src/jupyterhub_usage_quotas/manager.py
@@ -4,24 +4,55 @@ from typing import Any, Optional
 from tornado import web
 
 from jupyterhub_usage_quotas.client import HubAPIClient, PrometheusClient
-from jupyterhub_usage_quotas.config import UsageQuotaConfig
+from jupyterhub_usage_quotas.config import UsageQuotaConfig, policy_schema_backup
 
 
 class UsageQuotaManager(UsageQuotaConfig):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        hub_ip = self.config["JupyterHub"]["ip"]
-        hub_port = self.config["JupyterHub"]["port"]
+        hub_ip = self.config.get("JupyterHub", {}).get("ip", "127.0.0.1")
+        hub_port = self.config.get("JupyterHub", {}).get("port", 8000)
         self.hub_api_client = HubAPIClient(hub_url=f"http://{hub_ip}:{hub_port}")
         self.prometheus_client = PrometheusClient(self.prometheus_url)
+
+    def resolve_intersection(self, policies: list(dict), operator: str) -> dict:
+        #  TODO: add logic to match other required keys, e.g. units
+        resource_keys = policy_schema_backup["properties"]["resource"]["enum"]
+        policy_intersection = {}
+        for resource in resource_keys:
+            limit_values = []
+            for p in policies:
+                if p["resource"] == resource:
+                    limit_values.append(p["limit"]["value"])
+            if limit_values:
+                if operator == "min":
+                    limit = min(limit_values)
+                elif operator == "max":
+                    limit = max(limit_values)
+                elif operator == "sum":
+                    limit = sum(limit_values)
+                else:
+                    print("Operator not recognized.")
+                p["limit"]["value"] = limit
+                policy_intersection.update(p)
+        return policy_intersection
 
     async def resolve_policy(self, user):
         """
         Resolve which quota policy applies to the user.
         """
         data_user = await self.hub_api_client.query("users")
-        return data_user
+        entry_user = next(filter(lambda x: x["name"] == user, data_user), None)
+        groups_user = entry_user["groups"]
+        policies = [
+            p for p in self.policy if set(groups_user) <= set(p["scope"]["group"])
+        ]
+        test = self.resolve_intersection(
+            policies, self.scope_backup_strategy["intersection"]
+        )
+        print(f"{test=}")
+        return groups_user
 
     async def enforce(self, user):
         usage_metric = self.prometheus_usage_metrics["memory"]
