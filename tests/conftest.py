d9532e32010e967e669d8240de45a6b81aa0b5a6
jnywong <jnywong.pro@gmail.com>
2026-03-04 13:57:56 +0000

    refactor: remove hub and mock hub api

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,124 +1,16 @@
-import asyncio
+import json
 import logging
-import os
-import secrets
-import shutil
-import signal
-import time
-from functools import partial
-from subprocess import Popen
-from tempfile import TemporaryDirectory
-from unittest import mock
+from unittest.mock import AsyncMock
 
-import aiohttp
-import backoff
-import psutil
 import pytest
-from yarl import URL
 
 logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session")
-def admin_token():
-    """Generate a token to use for admin requests"""
-    token = secrets.token_hex(16)
-    # jupyterhub subprocess loads this from the environment
-    with mock.patch.dict(os.environ, {"TEST_PYTEST_TOKEN": token}):
-        yield token
-
-
-@pytest.fixture(scope="session")
-def hub_url():
-    # hardcoded for now, but might want to override
-    return "http://127.0.0.1:8000"
-
-
-@pytest.fixture(scope="session")
-async def hub(hub_url, request, admin_token):
+async def mock_hub_client():
     """Start JupyterHub, set up to use admin and service tokens"""
-    jupyterhub_config = os.path.dirname(__file__) + "/jupyterhub_config.py"
-    with TemporaryDirectory() as td:
-        shutil.copy(jupyterhub_config, td + "/jupyterhub_config.py")
-        hub = Popen(["jupyterhub", "--debug"], cwd=td)
-
-        def cleanup():
-            if hub.poll() is None:
-                try:
-                    kill_proc_tree(hub.pid)
-                except psutil.ProcessLookupError:
-                    pass
-            logger.info("terminating hub")
-            hub.terminate()
-
-        request.addfinalizer(cleanup)
-
-        deadline = time.monotonic() + 30
-        while time.monotonic() < deadline:
-            resp = await api_request(
-                hub_url=hub_url, path="hub/api/info", token=admin_token
-            )
-            if resp:
-                logger.info(f"{resp}")
-                logger.info("hub started")
-                break
-            else:
-                logger.info(f"{resp}")
-                logger.info("waiting for hub to start")
-                await asyncio.sleep(1)
-                continue
-
-        yield hub
-
-
-@backoff.on_exception(backoff.expo, aiohttp.ClientError, max_tries=8, logger=logger)
-async def api_request(
-    hub_url: str, path: str, token: str = None, parse_json: bool = True
-):
-    """Make an API request to the Hub, parsing JSON responses"""
-    headers = {"Authorization": f"token {token}"}
-    hub_url = URL(hub_url)
-    url = hub_url.with_path(f"{path}")
-    logger.info(f"Making request to {url}")
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as resp:
-            if resp.status == 200:
-                try:
-                    if parse_json:
-                        return await resp.json()
-                    else:
-                        return await resp.text()
-                except:
-                    logger.error("Failed to decode response.")
-                    raise
-            else:
-                raise aiohttp.ClientError(f"Bad response: {resp.status}")
-
-
-@pytest.fixture(scope="session")
-def admin_request(hub, hub_url, admin_token):
-    """make an API request to a path with an admin token"""
-    return partial(api_request, hub_url, token=admin_token)
-
-
-def kill_proc_tree(
-    pid, sig=signal.SIGKILL, include_parent=True, timeout=None, on_terminate=None
-):
-    """Kill a process tree (including grandchildren) with signal
-    "sig" and return a (gone, still_alive) tuple.
-    "on_terminate", if specified, is a callback function which is
-    called as soon as a child terminates.
-    """
-    assert pid != os.getpid(), "won't kill myself"
-    parent = psutil.Process(pid)
-    children = parent.children(recursive=True)
-    if include_parent:
-        children.append(parent)
-    for p in children:
-        try:
-            p.send_signal(sig)
-            logger.info(f"terminating {p}")
-        except psutil.NoSuchProcess:
-            pass
-    gone, alive = psutil.wait_procs(children, timeout=timeout, callback=on_terminate)
-    logger.info(f"{len(gone)} processes killed, {len(alive)} still alive.")
+    mock_hub_client = AsyncMock()
+    with open("tests/data/hub_api_user.json", "r") as f:
+        mock_hub_client.query.return_value = json.load(f)
+    yield mock_hub_client
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,12 +1,61 @@
 import logging
+from unittest.mock import MagicMock
 
 logger = logging.getLogger(__name__)
 
+from traitlets.config import Config
 
-async def test_hub_alive(admin_request):
-    """Test that the hub is alive and responding to requests."""
-    try:
-        response = await admin_request(path="hub/api/info")
-    except Exception:
-        raise RuntimeError("Hub is not alive")
-    assert response["version"] is not None
+from jupyterhub_usage_quotas.manager import UsageQuotaManager
+
+
+async def test_resolve_policy_single(mock_hub_client):
+    """
+    Test policy resolver applies a single policy scope for group-0 to user-0.
+    """
+    c = Config()
+    c.UsageQuotaManager.scope_backup_strategy = {
+        "empty": {
+            "resource": "memory",
+            "limit": {"value": 500, "unit": "GiB-hours"},
+            "window": 7,
+        },
+        "intersection": "min",
+    }
+    c.UsageQuotaManager.policy = (
+        {
+            "resource": "memory",
+            "limit": {
+                "value": 5000,
+                "unit": "GiB-hours",
+            },
+            "window": 30,
+            "scope": {"group": ["group-0"]},
+        },
+    )
+
+    quota_manager = UsageQuotaManager(config=c)
+    quota_manager.hub_api_client = mock_hub_client
+    merged = await quota_manager.resolve_policy("user-0")
+    assert merged == quota_manager.policy
+
+
+async def test_resolve_policy_empty(mock_hub_client):
+    """
+    Test policy resolver yields 'empty' backup strategy for user-2 who is a member of no groups.
+    """
+    c = Config()
+    c.UsageQuotaManager.scope_backup_strategy = {
+        "empty": {
+            "resource": "memory",
+            "limit": {"value": 500, "unit": "GiB-hours"},
+            "window": 7,
+        },
+        "intersection": "min",
+    }
+    quota_manager = UsageQuotaManager(config=c)
+    quota_manager.hub_api_client = mock_hub_client
+    quota_manager.resolve_empty = MagicMock(
+        return_value=c.UsageQuotaManager.scope_backup_strategy["empty"]
+    )
+    merged = await quota_manager.resolve_policy("user-2")
+    assert merged == quota_manager.scope_backup_strategy["empty"]
