6ed2c9ff3717ba171b8ee409adae9a8367a0b1ad
jnywong <jnywong.pro@gmail.com>
2026-03-10 15:27:52 +0000

    tests: enforce quotas

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,5 +1,5 @@
 import logging
-from unittest.mock import MagicMock, Mock
+from unittest.mock import Mock
 
 import kubespawner
 import pytest
@@ -22,7 +22,7 @@ class MockGroup(Mock):
 
 class MockUser(Mock):
     name = "test-user"
-    groups = [MockGroup()]
+    groups = []
     server = Server()
 
     def __init__(self, **kwargs):
@@ -108,9 +108,8 @@ async def test_resolve_policy_multiple():
 
 async def test_resolve_policy_empty():
     """
-    Test policy resolver yields 'empty' backup strategy for user-2 who is a member of no groups.
+    Test policy resolver yields 'empty' backup strategy.
     """
-    spawner = kubespawner.KubeSpawner(_mock=True, user=MockUser(name="user-2"))
     c = Config()
     c.UsageQuotaManager.scope_backup_strategy = {
         "empty": {
@@ -121,11 +120,8 @@ async def test_resolve_policy_empty():
         "intersection": "min",
     }
     quota_manager = UsageQuotaManager(config=c)
-    quota_manager.resolve_empty = MagicMock(
-        return_value=c.UsageQuotaManager.scope_backup_strategy["empty"]
-    )
-    merged = quota_manager.resolve_policy(spawner)
-    assert merged == quota_manager.scope_backup_strategy["empty"]
+    policy_empty = quota_manager.resolve_empty()
+    assert policy_empty == [quota_manager.scope_backup_strategy["empty"]]
 
 
 @pytest.mark.parametrize("operator", ["min", "max", "sum"])
@@ -184,3 +180,156 @@ async def test_resolve_policy_intersection(operator):
         assert merged[0]["limit"]["value"] == sum(
             [p["limit"]["value"] for p in quota_manager.policy]
         )
+
+
+async def test_enforce_single(mocker):
+    """
+    Test enforcing a single policy scope for group-0 to user-0.
+    """
+    spawner = kubespawner.KubeSpawner(
+        _mock=True, user=MockUser(name="user-0", groups=[MockGroup(name="group-0")])
+    )
+    c = Config()
+    c.UsageQuotaManager.scope_backup_strategy = {
+        "empty": {
+            "resource": "memory",
+            "limit": {"value": 500, "unit": "GiB-hours"},
+            "window": 7,
+        },
+        "intersection": "min",
+    }
+    c.UsageQuotaManager.policy = (
+        {
+            "resource": "memory",
+            "limit": {
+                "value": 5000,
+                "unit": "GiB-hours",
+            },
+            "window": 30,
+            "scope": {"group": ["group-0"]},
+        },
+    )
+    c.UsageQuotaManager.prometheus_usage_metrics = {
+        "memory": "kube_pod_container_resource_requests{resource='memory'}",
+    }
+    # Under quota limit
+    mock_usage = mocker.AsyncMock(return_value=[1773089003.938, "4999"])
+    mocker.patch(
+        "jupyterhub_usage_quotas.manager.UsageQuotaManager.get_usage", mock_usage
+    )
+    quota_manager = UsageQuotaManager(config=c)
+    output = await quota_manager.enforce(spawner)
+    assert output["allow_server_launch"] == True
+    # Over quota limit
+    mock_usage = mocker.AsyncMock(return_value=[1773089003.938, "5001"])
+    mocker.patch(
+        "jupyterhub_usage_quotas.manager.UsageQuotaManager.get_usage", mock_usage
+    )
+    quota_manager = UsageQuotaManager(config=c)
+    output = await quota_manager.enforce(spawner)
+    assert output["allow_server_launch"] == False
+
+
+async def test_enforce_empty(mocker):
+    """
+    Test enforcing empty policy scope for user-2 who is a member of no groups.
+    """
+    spawner = kubespawner.KubeSpawner(
+        _mock=True, user=MockUser(name="user-2", groups=[])
+    )
+    c = Config()
+    c.UsageQuotaManager.scope_backup_strategy = {
+        "empty": {
+            "resource": "memory",
+            "limit": {"value": 500, "unit": "GiB-hours"},
+            "window": 7,
+        },
+        "intersection": "min",
+    }
+    c.UsageQuotaManager.prometheus_usage_metrics = {
+        "memory": "kube_pod_container_resource_requests{resource='memory'}",
+    }
+    # Under quota limit
+    mock_usage = mocker.AsyncMock(return_value=[1773089003.938, "499"])
+    mocker.patch(
+        "jupyterhub_usage_quotas.manager.UsageQuotaManager.get_usage", mock_usage
+    )
+    quota_manager = UsageQuotaManager(config=c)
+    output = await quota_manager.enforce(spawner)
+    assert output["allow_server_launch"] == True
+    # Over quota limit
+    mock_usage = mocker.AsyncMock(return_value=[1773089003.938, "501"])
+    mocker.patch(
+        "jupyterhub_usage_quotas.manager.UsageQuotaManager.get_usage", mock_usage
+    )
+    quota_manager = UsageQuotaManager(config=c)
+    output = await quota_manager.enforce(spawner)
+    assert output["allow_server_launch"] == False
+
+
+@pytest.mark.parametrize(
+    "operator, under, over",
+    [
+        pytest.param("min", "699", "701"),
+        pytest.param("max", "4999", "5001"),
+        pytest.param("sum", "5699", "5701"),
+    ],
+)
+async def test_enforce_intersection(mocker, operator, under, over):
+    """
+    Test enforcing min/max/sum operator to policy scopes for user-3 who is a member of multiple groups, (group-0 and group-1).
+    """
+    spawner = kubespawner.KubeSpawner(
+        _mock=True,
+        user=MockUser(
+            name="user-1", groups=[MockGroup(name="group-0"), MockGroup(name="group-1")]
+        ),
+    )
+    c = Config()
+    c.UsageQuotaManager.scope_backup_strategy = {
+        "empty": {
+            "resource": "memory",
+            "limit": {"value": 500, "unit": "GiB-hours"},
+            "window": 7,
+        },
+        "intersection": operator,
+    }
+    c.UsageQuotaManager.policy = (
+        {
+            "resource": "memory",
+            "limit": {
+                "value": 5000,
+                "unit": "GiB-hours",
+            },
+            "window": 30,
+            "scope": {"group": ["group-0"]},
+        },
+        {
+            "resource": "memory",
+            "limit": {
+                "value": 700,
+                "unit": "GiB-hours",
+            },
+            "window": 30,
+            "scope": {"group": ["group-1"]},
+        },
+    )
+    c.UsageQuotaManager.prometheus_usage_metrics = {
+        "memory": "kube_pod_container_resource_requests{resource='memory'}",
+    }
+    # Under quota limit
+    mock_usage = mocker.AsyncMock(return_value=[1773089003.938, under])
+    mocker.patch(
+        "jupyterhub_usage_quotas.manager.UsageQuotaManager.get_usage", mock_usage
+    )
+    quota_manager = UsageQuotaManager(config=c)
+    output = await quota_manager.enforce(spawner)
+    assert output["allow_server_launch"] == True
+    # Over quota limit
+    mock_usage = mocker.AsyncMock(return_value=[1773089003.938, over])
+    mocker.patch(
+        "jupyterhub_usage_quotas.manager.UsageQuotaManager.get_usage", mock_usage
+    )
+    quota_manager = UsageQuotaManager(config=c)
+    output = await quota_manager.enforce(spawner)
+    assert output["allow_server_launch"] == False
