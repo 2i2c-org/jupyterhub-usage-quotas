2ee9ffc0ea7036d62b4861ac6a126fc10c8a08e3
jnywong <jnywong.pro@gmail.com>
2026-04-09 10:58:53 +0100

    feat: add setup_usage_quotas function

--- src/jupyterhub_usage_quotas/__init__.py
+++ src/jupyterhub_usage_quotas/__init__.py
@@ -8,3 +8,18 @@ __all__ = ["get_template_path", "UsageHandler"]
 def get_template_path():
     """Get the path to the templates directory."""
     return os.path.join(os.path.dirname(__file__), "templates")
+
+
+def setup_usage_quotas(c):
+    """
+    Setup common config to enable the usage quotas system.
+
+    Expects to be called from a `jupyterhub_config.py` file, with `c`
+    the config object being passed in.
+    """
+
+    c.JupyterHub.template_paths = [get_template_path()]
+
+    c.JupyterHub.extra_handlers = [
+        (r"/usage", UsageHandler),
+    ]
