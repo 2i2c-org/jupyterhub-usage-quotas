0e4e17018a4e8fb3b04f4bad6258343d9a7062d0
jnywong <jnywong.pro@gmail.com>
2026-04-10 12:07:12 +0100

    feat: get all users and groups

--- src/jupyterhub_usage_quotas/metrics.py
+++ src/jupyterhub_usage_quotas/metrics.py
@@ -1,19 +1,27 @@
 import os
 
 from jupyterhub_usage_quotas.client import HubApiClient
-from jupyterhub_usage_quotas.config import UsageQuotaConfig
+from jupyterhub_usage_quotas.config import UsageViewerConfig
 
 
-class MetricsExporter(UsageQuotaConfig):
+class MetricsExporter(UsageViewerConfig):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         api_url = os.environ.get("JUPYTERHUB_API_URL")
         api_token = os.environ.get("JUPYTERHUB_API_TOKEN")
         self.client = HubApiClient(api_url=api_url, token=api_token)
 
+    async def get_users_and_groups(self) -> list:
+        response = await self.client.query(path="users")
+        filtered = [
+            {"user_name": r.get("name"), "user_group": r.get("groups")}
+            for r in response
+        ]
+        return filtered
+
     async def export_metrics(self):
         """
         Export usage and quota limits as Prometheus metrics.
         """
-        response = await self.client.query(path="users")
-        self.log.info(response)
+        users_and_groups = await self.get_users_and_groups()
+        print(users_and_groups)
