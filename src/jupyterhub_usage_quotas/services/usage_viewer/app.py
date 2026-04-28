111ae921ede94f6c9b5c363e551ca2b7c1769bf3
jnywong <jnywong.pro@gmail.com>
2026-04-28 09:40:21 +0100

    feat: get compute data for usage viewer

--- src/jupyterhub_usage_quotas/services/usage_viewer/app.py
+++ src/jupyterhub_usage_quotas/services/usage_viewer/app.py
@@ -97,7 +97,8 @@ def create_fastapi_app(
 
         # Use storage client to get usage data
         usage_data = await storage_client.get_user_storage_usage(user["name"])
-
+        compute_data = await storage_client.get_user_compute_usage(user["name"])
+        print(compute_data)
         template = jinja_env.get_template("usage.html")
         html_content = template.render(usage_data=usage_data)
         return HTMLResponse(html_content)
