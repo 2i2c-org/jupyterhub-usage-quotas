bea4cfed3bcde5fd6e8dbbad3b7bcef56726093f
jnywong <jnywong.pro@gmail.com>
2026-05-13 14:34:18 +0100

    refactor: lift config into setup function

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -5,8 +5,6 @@ Example configuration file for JupyterHub usage quotas.
 import socket
 
 from jupyterhub_usage_quotas import setup_usage_quotas
-from jupyterhub_usage_quotas.manager import SpawnException, UsageQuotaManager
-from jupyterhub_usage_quotas.metrics import MetricsExporter
 
 c = get_config()  # noqa
 
@@ -35,24 +33,10 @@ c.Authenticator.admin_users = {"admin"}
 
 # Usage Quotas
 
-# Set up common Usage config
-setup_usage_quotas(c)
-
 c.UsageConfig.prometheus_auth = {"username": "", "password": ""}
 c.UsageConfig.hub_namespace = "staging"
 
 # Usage Quota Config
-
-c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
-
-c.UsageQuotaManager.prometheus_usage_metrics = {
-    "memory": "kube_pod_container_resource_requests{resource='memory'}",
-    # "cpu": "kube_pod_container_resource_requests{resource='cpu'}"
-}
-
-c.UsageQuotaManager.prometheus_scrape_interval = 60
-
-c.UsageQuotaManager.hub_namespace = "staging"
 
 c.UsageQuotaManager.scope_backup_strategy = {
     "empty": {
@@ -118,40 +102,5 @@ c.JupyterHub.load_roles = [
     },
 ]
 
-# KubeSpawner
-
-c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
-
-quota_manager = UsageQuotaManager(config=c)
-
-
-async def quota_pre_spawn_hook(spawner):
-    try:
-        user_name = spawner.user.name
-        user_groups = [g.name for g in spawner.user.groups]
-        output = await quota_manager.enforce(user_name, user_groups)
-        launch_flag = output["allow_server_launch"]
-    except Exception as e:
-        if c.UsageQuotaManager.failover_open is True:
-            launch_flag = True
-            quota_manager.log.error(
-                f"Usage quota system failed open for user {user_name} with exception: {e}."
-            )
-        else:
-            raise SpawnException(
-                status_code=424,
-                log_message="Spawn failure occurred due to a failed dependency in the usage quota system. Please contact your hub admin for assistance.",
-            )
-    if launch_flag is False:
-        raise SpawnException(
-            status_code=422,
-            log_message=f"{output['error']['message']}",
-            html_message=f"<p>Compute {output['quota']['resource']} quota limit exceeded.</p><p style='font-size:100%'>You have used <span style='color:var(--bs-red)'>{output['quota']['used']:.2f}</span> / {output['quota']['limit']['value']:.2f} {output['quota']['limit']['unit']} in the last {output['quota']['window']} days.</p><p style='font-size:100%'>Your quota will reset on <b><time datetime='{output['error']['retry_time']}'>{output['error']['retry_time']}</time></b>.</p><p style='font-size:100%'>Contact your JupyterHub admin if you need additional quota.</p><i style='font-size:100%;color:var(--bs-gray)'>Last updated: <time datetime='{output['timestamp']}'>{output["timestamp"]}</time>.</i>",
-        )
-
-
-c.KubeSpawner.pre_spawn_hook = quota_pre_spawn_hook
-
-metrics_exporter = MetricsExporter(quota_manager)
-
-metrics_exporter.start()
+# Set up common usage quotas config
+setup_usage_quotas(c)
--- src/jupyterhub_usage_quotas/__init__.py
+++ src/jupyterhub_usage_quotas/__init__.py
@@ -1,6 +1,8 @@
 import os
 
 from jupyterhub_usage_quotas.handler import UsageHandler
+from jupyterhub_usage_quotas.manager import SpawnException, UsageQuotaManager
+from jupyterhub_usage_quotas.metrics import MetricsExporter
 
 __all__ = ["get_template_path", "UsageHandler"]
 
@@ -21,3 +23,42 @@ def setup_usage_quotas(c):
     c.JupyterHub.template_paths.insert(0, get_template_path())
 
     c.JupyterHub.extra_handlers.append((r"/usage", UsageHandler))
+
+    c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
+
+    c.UsageQuotaManager.prometheus_usage_metrics = {
+        "memory": "kube_pod_container_resource_requests{resource='memory'}",
+        # "cpu": "kube_pod_container_resource_requests{resource='cpu'}"
+    }
+
+    quota_manager = UsageQuotaManager(config=c)
+
+    async def quota_pre_spawn_hook(spawner):
+        try:
+            user_name = spawner.user.name
+            user_groups = [g.name for g in spawner.user.groups]
+            output = await quota_manager.enforce(user_name, user_groups)
+            launch_flag = output["allow_server_launch"]
+        except Exception as e:
+            if c.UsageQuotaManager.failover_open is True:
+                launch_flag = True
+                quota_manager.log.error(
+                    f"Usage quota system failed open for user {user_name} with exception: {e}."
+                )
+            else:
+                raise SpawnException(
+                    status_code=424,
+                    log_message="Spawn failure occurred due to a failed dependency in the usage quota system. Please contact your hub admin for assistance.",
+                )
+        if launch_flag is False:
+            raise SpawnException(
+                status_code=422,
+                log_message=f"{output['error']['message']}",
+                html_message=f"<p>Compute {output['quota']['resource']} quota limit exceeded.</p><p style='font-size:100%'>You have used <span style='color:var(--bs-red)'>{output['quota']['used']:.2f}</span> / {output['quota']['limit']['value']:.2f} {output['quota']['limit']['unit']} in the last {output['quota']['window']} days.</p><p style='font-size:100%'>Your quota will reset on <b><time datetime='{output['error']['retry_time']}'>{output['error']['retry_time']}</time></b>.</p><p style='font-size:100%'>Contact your JupyterHub admin if you need additional quota.</p><i style='font-size:100%;color:var(--bs-gray)'>Last updated: <time datetime='{output['timestamp']}'>{output["timestamp"]}</time>.</i>",
+            )
+
+    c.KubeSpawner.pre_spawn_hook = quota_pre_spawn_hook
+
+    # Start Prometheus metrics exporter
+    metrics_exporter = MetricsExporter(quota_manager)
+    metrics_exporter.start()
