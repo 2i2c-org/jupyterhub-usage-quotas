c52740bf477d316833f36928d3ebe92796cc9824
jnywong <jnywong.pro@gmail.com>
2026-05-14 11:28:26 +0100

    tests: update

--- tests/services/fixtures/tornado_app.py
+++ tests/services/fixtures/tornado_app.py
@@ -70,5 +70,6 @@ class UsageViewerTestCase(AsyncHTTPTestCase):
         config = UsageViewerConfig()
         config.service_prefix = "/services/usage-quota/"
         config.public_hub_url = "http://test-hub:8000"
+        config.enable_component = {"home_storage": True, "compute": True}
         config.session_secret_key = "0" * 64
         return make_app(client, config)
